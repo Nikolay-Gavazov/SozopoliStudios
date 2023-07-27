edd1fdd3081bcba2d4cd66f210daf52f9eb33aa4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-27 20:19:04 +0300

    Create studio.module.ts

---
+++ src/app/studio/studio.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudioRoutingModule } from './studio-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StudioRoutingModule
+  ]
+})
+export class StudioModule { }
