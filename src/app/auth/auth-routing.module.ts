91c6a2db157346ad4c99c39ba4d09a8c64873c69
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-27 20:18:17 +0300

    Create auth-routing.module.ts

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'logout', component: LogoutComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
