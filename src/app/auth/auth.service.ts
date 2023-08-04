8ea864595b1270dd4612db74788877717e2f5cfa
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-04 22:50:55 +0300

    Update auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,12 +4,12 @@ import { User } from '../types/user';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-const {apiUrl} = environment;
+const { apiUrl } = environment;
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements OnDestroy{
+export class AuthService implements OnDestroy {
   private user$$ = new BehaviorSubject<User | undefined>(undefined);
   public user$ = this.user$$.asObservable();
 
@@ -18,28 +18,28 @@ export class AuthService implements OnDestroy{
 
   subscription: Subscription;
   constructor(private http: HttpClient) {
-    this.subscription = this.user$.subscribe((user) =>{
+    this.subscription = this.user$.subscribe((user) => {
       this.user = user;
     })
-   }
+  }
 
-   login(email: string, password: string){
+  login(email: string, password: string) {
     return this.http
-    .post<User>(`/api/login`, { email, password})
-    .pipe(tap(user => this.user$$.next(user)));
-   }
+      .post<User>(`/api/login`, { email, password })
+      .pipe(tap(user => this.user$$.next(user)));
+  }
 
-   register(email: string, password: string){
-    return this.http.post<User>(`${apiUrl}/register`, { email:email, password:password}).pipe(tap(user => this.user$$.next(user)));
-   }
+  register(email: string, password: string) {
+    return this.http.post<User>(`${apiUrl}/register`, { email: email, password: password }).pipe(tap(user => this.user$$.next(user)));
+  }
 
-   logout(){
-    return this.http.post<User>(`${apiUrl}/logout` ,{}).pipe(tap(() => this.user$$.next(undefined)));
-   }
+  logout() {
+    return this.http.post<User>(`${apiUrl}/logout`, {}).pipe(tap(() => this.user$$.next(undefined)));
+  }
 
-   get isLoggedIn(){
+  get isLoggedIn() {
     return !!this.user;
-   }
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
