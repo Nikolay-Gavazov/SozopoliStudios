3e403eac214ff048c3408da6dd37706e9427f914
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-06 00:01:35 +0300

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { RouterModule } from '@angular/router';
 
 
 
@@ -21,6 +21,6 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule
   ],
-  exports:[HeaderComponent,FooterComponent,NotFoundComponent]
+  exports: [HeaderComponent, FooterComponent, NotFoundComponent]
 })
 export class CoreModule { }
