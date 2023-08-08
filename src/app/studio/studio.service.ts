6bbe78693d7125654d0cd69f90c2a9175465f890
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-08 19:00:43 +0300

    create studio service

---
+++ src/app/studio/studio.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Studio } from '../shared/types/studio';
+import { environment } from 'src/environments/environment';
+
+const { apiUrl } = environment;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudioService {
+  
+  constructor(private http: HttpClient) { }
+
+  getStudios(){
+    return this.http.get<Studio[]>(`${apiUrl}/studios`)
+  }
+}
