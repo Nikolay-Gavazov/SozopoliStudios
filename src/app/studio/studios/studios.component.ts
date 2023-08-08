bb1570824f692143952a80b48b5d0bdc70ad14d9
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-08 19:03:10 +0300

    made studios page

--- src/app/studio/studios/studios.component.ts
+++ src/app/studio/studios/studios.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Studio } from 'src/app/shared/types/studio';
+import { StudioService } from '../studio.service';
 
 @Component({
   selector: 'app-studios',
   templateUrl: './studios.component.html',
   styleUrls: ['./studios.component.css']
 })
-export class StudiosComponent {
+export class StudiosComponent implements OnInit{
+  studiosList: Studio[] = [];
+  studio1: Studio[] = [];
+  isLoading: boolean = true;
 
+  constructor(private studioService: StudioService){}
+
+  ngOnInit(): void {
+    this.studioService.getStudios().subscribe({
+      next:(studios) =>{
+        this.isLoading = false;
+        this.studiosList = studios;
+        this.studio1 = this.studiosList.splice(0,1);  
+        console.log(this.studio1);
+        
+      },
+      error:(error) =>{
+        this.isLoading = false;
+        console.log(error);
+        
+      }
+    })
+  }
 }
