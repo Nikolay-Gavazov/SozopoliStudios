eaf90f0b3e5247c07426da82e8bea124c9b2f492
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-09 00:45:02 +0300

    add current date to form and options of studios

--- src/app/studio/booking/booking.component.ts
+++ src/app/studio/booking/booking.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Studio } from 'src/app/shared/types/studio';
+import { StudioService } from '../studio.service';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit{
+  studiosList: Studio[] = [];
+  isLoading: boolean = true;
 
+  constructor(private studioService: StudioService){}
+
+  ngOnInit(): void {
+    this.studioService.getStudios().subscribe({
+      next:(studios) =>{
+        this.isLoading = false;
+        this.studiosList = studios;
+      },
+      error:(error) =>{
+        this.isLoading = false;
+        console.log(error);
+      }
+    })
+  }
+  date = new Date();
+
+  submit(form: NgForm){
+    console.log(form.value);
+    
+  }
 }
