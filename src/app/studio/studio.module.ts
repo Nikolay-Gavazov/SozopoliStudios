1afa7d0919719acbaebccccf45b1968d5ee5d9d7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-09 00:45:24 +0300

    add  booking component

--- src/app/studio/studio.module.ts
+++ src/app/studio/studio.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudioRoutingModule } from './studio-routing.module';
-
+import { FormsModule } from '@angular/forms';
+import { BookingComponent } from './booking/booking.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookingComponent
+  ],
   imports: [
     CommonModule,
-    StudioRoutingModule
+    StudioRoutingModule,
+    FormsModule
   ]
 })
 export class StudioModule { }
