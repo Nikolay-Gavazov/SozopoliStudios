31089ccbeacd8551e6d11ca80fb06ebd0543e7d4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-10 18:39:31 +0300

    add guards

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RegisterComponent } from './register/register.component';
+import { GuestGuard } from '../core/guards/guestGuard';
+import { UserGuard } from '../core/guards/userGuard';
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'logout', component: LogoutComponent },
+    { path: 'login', component: LoginComponent, canActivate:[GuestGuard] },
+    { path: 'register', component: RegisterComponent, canActivate:[GuestGuard] },
+    { path: 'logout', component: LogoutComponent,canActivate:[UserGuard] },
 ];
 
 @NgModule({
