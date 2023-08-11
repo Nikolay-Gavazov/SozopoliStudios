728496de24898e073b6b24434835c71e16fe30bd
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-12 01:24:28 +0300

    Create adminGuard.ts

---
+++ src/app/core/guards/adminGuard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
+
+
+@Injectable({ providedIn: 'root' })
+
+export class AdminGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):boolean | UrlTree{
+        if(this.authService.isLoggedIn){
+            if(this.authService.userData.isAdmin){
+                return true;
+            }
+        }
+        return this.router.createUrlTree(["/"])
+    }
+}
