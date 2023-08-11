4602bacffe967ff5455d25ee86dc042b1916cace
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-12 01:12:18 +0300

    change authtorization method

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -2,19 +2,31 @@ import { Injectable, Provider } from "@angular/core";
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment.development";
+import { AuthService } from "./auth/auth.service";
 
 const {apiUrl} = environment;
 @Injectable()
 export class AppInterceptor implements HttpInterceptor{
+    constructor(private authService: AuthService){
+    }
     intercept(
         req: HttpRequest<any>, 
         next: HttpHandler
         ): Observable<HttpEvent<any>> {
+        const userData = this.authService.userData;
+        if(userData){
+            req = req.clone({
+                setHeaders:{'X-Authorization':userData.token}
+            })
+        }else{
+            req = req.clone({
+                setHeaders:{'Content-Type':'application/json'}
+            })
+        }
         if(req.url.startsWith('/api')){
             
-            req= req.clone({
+            req = req.clone({
                 url:req.url.replace('/api',apiUrl),
-                withCredentials:true
             })  
         }
         return next.handle(req);
