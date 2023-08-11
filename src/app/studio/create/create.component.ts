ca602d796b08b32c975429bde6f03a16b2c4f12c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-12 01:25:34 +0300

    add create component

---
+++ src/app/studio/create/create.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StudioService } from '../studio.service';
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent {
+  constructor(private studioService: StudioService, private router: Router){
+
+  }
+  submit(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    const {studioName, img, description} = form.value;
+    this.studioService.createStudio(studioName, img, description).subscribe((response)=>{
+      this.router.navigate(["/"])
+    });
+  }
+}
