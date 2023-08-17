5e2d69f17a946b0b41952db45223bddeb264a7d6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-17 23:58:15 +0300

    Update studio.service.ts

--- src/app/studio/studio.service.ts
+++ src/app/studio/studio.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Studio } from '../shared/types/studio';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
 
 const { apiUrl } = environment;
 
@@ -10,23 +9,23 @@ const { apiUrl } = environment;
   providedIn: 'root'
 })
 export class StudioService {
-  
+
   constructor(private http: HttpClient) { }
 
-  getStudios(){
+  getStudios() {
     return this.http.get<Studio[]>(`${apiUrl}/studios`)
   }
-  getStudio(id: string){
+  getStudio(id: string) {
     return this.http.get<Studio[]>(`${apiUrl}/studios/${id}`)
   }
-  editStudio(id: string,{studioName, img, description}:any){
-    return this.http.put<Studio>(`${apiUrl}/studios/edit/${id}`,{studioName, img, description})
+  editStudio(id: string, { studioName, img, description }: any) {
+    return this.http.put<Studio>(`${apiUrl}/studios/edit/${id}`, { studioName, img, description })
   }
-  createStudio(studioName: string, img: string, description:string){
-    const headers = new HttpHeaders({'Content-Type': 'application/json'})
-    return this.http.post(`${apiUrl}/studios`,{studioName, img, description})
+  createStudio(studioName: string, img: string, description: string) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' })
+    return this.http.post(`${apiUrl}/studios`, { studioName, img, description })
   }
-  deleteStudio(id:string){
+  deleteStudio(id: string) {
     return this.http.delete(`${apiUrl}/studios/delete/${id}`);
   }
 }
