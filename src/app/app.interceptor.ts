e3242addaf39b16852df423cd845ce141cab6b11
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-17 23:54:35 +0300

    Update app.interceptor.ts

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -4,24 +4,24 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment.development";
 import { AuthService } from "./auth/auth.service";
 
-const {apiUrl} = environment;
+const { apiUrl } = environment;
 @Injectable()
-export class AppInterceptor implements HttpInterceptor{
-    constructor(private authService: AuthService){
+export class AppInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {
     }
     intercept(
-        req: HttpRequest<any>, 
+        req: HttpRequest<any>,
         next: HttpHandler
-        ): Observable<HttpEvent<any>> {
-        if(req.url.startsWith('/api')){
-            
+    ): Observable<HttpEvent<any>> {
+        if (req.url.startsWith('/api')) {
+
             req = req.clone({
-                url:req.url.replace('/api',apiUrl),
-            })  
+                url: req.url.replace('/api', apiUrl),
+            })
         }
         return next.handle(req);
-        }
     }
+}
 export const appInterceptorProvider: Provider = {
     multi: true,
     useClass: AppInterceptor,
