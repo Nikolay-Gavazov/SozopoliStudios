28e70e0264cfe347957a74ec7a69421b1635dc26
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-17 23:59:20 +0300

    Update studio-routing.module.ts

--- src/app/studio/studio-routing.module.ts
+++ src/app/studio/studio-routing.module.ts
@@ -5,21 +5,21 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { StudiosComponent } from './studios/studios.component';
 import { DetailsComponent } from './details/details.component';
 import { UserGuard } from '../core/guards/userGuard';
-import {AdminGuard} from '../core/guards/adminGuard'
+import { AdminGuard } from '../core/guards/adminGuard'
 import { EditComponent } from './edit/edit.component';
 import { CreateComponent } from './create/create.component';
 import { NotFoundComponent } from '../core/not-found/not-found.component';
 
 
 const routes: Routes = [
-    {path: '', component: StudiosComponent},
-    {path: 'create', component: CreateComponent , canActivate:[AdminGuard]},
-  {path: 'gallery', component: GalleryComponent},
-  {path: 'book', component: BookingComponent, canActivate:[UserGuard]},
-  {path: ':id', component: DetailsComponent},
-  {path: 'edit/:id', component: EditComponent, canActivate:[AdminGuard]},
-  {path: 'delete/:id', component: EditComponent, canActivate:[AdminGuard]},
-  {path: "*", component: NotFoundComponent}
+  { path: '', component: StudiosComponent },
+  { path: 'create', component: CreateComponent, canActivate: [AdminGuard] },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'book', component: BookingComponent, canActivate: [UserGuard] },
+  { path: ':id', component: DetailsComponent },
+  { path: 'edit/:id', component: EditComponent, canActivate: [AdminGuard] },
+  { path: 'delete/:id', component: EditComponent, canActivate: [AdminGuard] },
+  { path: "*", component: NotFoundComponent }
 ];
 
 @NgModule({
