679265a593d504a5bc12fb9663995c2204fbe468
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-20 03:26:48 +0300

    Update user.ts

--- src/app/shared/types/user.ts
+++ src/app/shared/types/user.ts
@@ -1,10 +1,10 @@
-export interface User{
+export interface User {
     email: string;
     password: string;
     _id: string;
     created_at: string;
     updated_at: string;
-    __v:number;
+    __v: number;
 };
 
 export interface UserToken {
