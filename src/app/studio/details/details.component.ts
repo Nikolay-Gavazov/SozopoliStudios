ac86b0cd241ac18d1c10c99c75e5eea148bfaa0e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-20 03:26:38 +0300

    Update details.component.ts

--- src/app/studio/details/details.component.ts
+++ src/app/studio/details/details.component.ts
@@ -8,31 +8,31 @@ import { AuthService } from 'src/app/auth/auth.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit{
-  studio:any;
+export class DetailsComponent implements OnInit {
+  studio: any;
   isLoading: boolean = true;
   isAdmin: boolean = false;
-  id!:string;
-  deleteStudio():void {
-   const confirmation = confirm("Are you sure you want to delete");
-    if(confirmation){
-      this.studioService.deleteStudio(this.id).subscribe((response) =>{
+  id!: string;
+  deleteStudio(): void {
+    const confirmation = confirm("Are you sure you want to delete");
+    if (confirmation) {
+      this.studioService.deleteStudio(this.id).subscribe((response) => {
         this.router.navigate(['/']);
       })
     }
   }
-  constructor(private studioService: StudioService, private activatedRoute: ActivatedRoute, private authService: AuthService, private router: Router){}
+  constructor(private studioService: StudioService, private activatedRoute: ActivatedRoute, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.params['id'] || '';
-    
-    this.studioService.getStudio(this.id).subscribe((studio) =>{
-        this.isLoading = false;
-        this.studio = studio;
-        if(this.authService.userData){
-          this.isAdmin = this.authService.userData.isAdmin;
-        }
-      })
+
+    this.studioService.getStudio(this.id).subscribe((studio) => {
+      this.isLoading = false;
+      this.studio = studio;
+      if (this.authService.userData) {
+        this.isAdmin = this.authService.userData.isAdmin;
+      }
+    })
   }
-  
+
 }
